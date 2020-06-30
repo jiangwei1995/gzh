1b144bfe9deede54de3756074df0e543760d718d
jiangwei <jiangwei@gagogroup.com>
2020-06-30 19:39:41 +0800

    init

--- app.js
+++ app.js
@@ -9,15 +9,8 @@ app.post('/gzh/tools/msg', (req, res) => {
         error: 0
     });
 });
-app.get('/gzh/tools/accessToken', async (req, res) => {
-    console.log(req.body);
-    const appid = process.env["APP_ID"];
-    const secret =  process.env["SECRET"];
-    const gzh = new Gzh(appid, secret);
-    const result = await gzh.access_token();
-    res.send({
-        access_token: result.access_token
-    });
+app.get('/gzh/tools/msg', async (req, res) => {
+    res.send(result.access_token);
 });
 
 app.listen(3000, () => {
